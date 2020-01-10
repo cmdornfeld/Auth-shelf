a928cf189c76c58bbd8963e5c33fb5f0d6fe5aa6
Chad Dornfeld <cmdornfeld@gmail.com>
2020-01-10 10:55:13 -0600

    updated map on ShelfPage.js to include key for item: description

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -3,28 +3,27 @@ import {connect} from 'react-redux';
 
 class ShelfList extends Component {
 
-    // componentDidMount() {
-    //     this.props.dispatch({type: 'GET_ITEMS'});
-    // }
+    componentDidMount() {
+        this.props.dispatch({type: 'GET_ITEMS'});
+    }
 
     render() {
         return (
             <>
-            {JSON.stringify(this.props)}
-            {/* <ul>
+            <ul>
             {this.props.getItem.map((item, i) => {
                 return (
-                    <li key={i}>{item}</li>
+                    <li key={i}>{item.description}</li>
                 )})
             }
-            </ul> */}
+            </ul>
             </>
         )
     }
 }
 
 const putReduxStateOnProps = (reduxStore) => ({
-    reduxStore
+    getItem: reduxStore.getItem
 })
 
 export default connect(putReduxStateOnProps)(ShelfList);
